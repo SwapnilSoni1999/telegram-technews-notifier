bc71498b2efb7d41a70cc71b808b3500dcacc66b
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2019-11-04 18:10:36 +0530

    setup for gadgetsnow

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import time
-from sites import (beebom, digit)
+from sites import (beebom, digit, gadgetsnow)
 
 # initiate all sites
 beebom = beebom.Beebom()
 digit = digit.Digit()
+gadgetsnow = gadgetsnow.GadgetsNow()
 
 # get all posts from all sites
 beebom_post = beebom.latest_post()
 digit_post = digit.latest_news()
+gadgetsnow_post = gadgetsnow.get_latest_news()
 
 
 while True:
     # Beebom
-    curr_time = int(time.time())
+    # curr_time = int(time.time())
     new_post_beebom = beebom.latest_post()
     if new_post_beebom['time'] > beebom_post['time']:
         print('Found new post on Beebom!', new_post_beebom)
@@ -29,7 +31,13 @@ while True:
     else:
         print('No new post found on digit')
 
-    
+    # GadgetsNow
+    new_post_gn = gadgetsnow.get_latest_news()
+    if new_post_gn['time'] > gadgetsnow_post['time']:
+        print('New post found on Gadgets Now', new_post_gn)
+        gadgetsnow_post = new_post_gn
+    else:
+        print('No new post found on GadgetsNow!')
     
     time.sleep(60)
     
---
+++ sites/gadgetsnow.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+
+class GadgetsNow:
+    def __init__(self):
+        pass
+
+    def get_latest_news(self):
+
+        def getTime(s):
+            taim = datetime.strptime(s, '%d %b %Y, %H:%M')
+            return int(taim.timestamp())
+
+
+        res = requests.get('https://www.gadgetsnow.com/latest-news')
+        soup = BeautifulSoup(res.content, 'lxml')
+        news_section = soup.find('div', { 'class': [ 'tech_list', 'ctn_stories' ] })
+        top_post = news_section.find('li')
+        return {
+            'title': top_post.find('a')['title'],
+            'url': top_post.find('a')['href'],
+            'time': getTime(top_post.find('span', { 'class': 'w_bylinec' })['rodate'])
+        }
