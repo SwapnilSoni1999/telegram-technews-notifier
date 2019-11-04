eaeeffed6eae77265fecb1e68d213a529fe23408
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2019-11-05 04:17:32 +0530

    add for androidauthority

---
+++ sites/androidauthority.py
@@ -0,0 +1,21 @@
+import cfscrape
+from bs4 import BeautifulSoup
+
+class AndroidAuthority:
+    def __init__(self):
+        pass
+
+    def latest_news(self):
+        scraper = cfscrape.create_scraper()
+        res = scraper.get('https://www.androidauthority.com/news/')
+            
+        soup = BeautifulSoup(res.content, 'lxml')
+        top_news = soup.find('div', { 'id': 'page' })
+        # print(top_news)
+        first_news = top_news.find('div', { 'class': [ 'aa_hfp_container', 'add-active' ] })
+        # print(first_news)
+
+        return {
+            'title': first_news.find('h3').text,
+            'url': first_news.find('a', { 'class': 'overlay-link' })['href']
+        }
